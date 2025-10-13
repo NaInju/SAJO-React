c6f97e12978c41d5cc631ee2f3299fccd50006b1
Inzoo <skwn.naver.com@gmail.com>
2025-10-13 15:51:24 +0900

    fix: update index.html script path for GitHub Pages2

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './styles/reset.css';
+import './styles/main.css';   // (이미 추가했으면 OK)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
