870e58b4e2bcccf4cbb9ccdc6a037e3a3edb6ee7
Inzoo <skwn.naver.com@gmail.com>
2025-10-15 15:26:51 +0900

    prod-desc 수정

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -256,20 +256,32 @@ function ProductInfo() {
                   <div className="prod-img">
                     <img src="./img/products/grid_visual/sajotuna_can_product_1000002397_detail_078.png" alt="사조참치" />
                     <div className="prod-overlay">
-                      담백한 참치 본연의 맛을 살린 정통 참치.<br/>
-                      샐러드나 김밥 등 다양한 요리에 잘 어울립니다.</div>
+                      <strong className='prod-name'>사조참치 살코기</strong>
+                      <p className='prod-desc'>
+                        담백한 참치 본연의 맛을 살린 정통 참치.<br/>
+                        샐러드나 김밥 등 다양한 요리에 잘 어울립니다.
+                      </p>
+                    </div>
                   </div>
                   <div className="prod-img">
                     <img src="./img/products/grid_visual/bluetuna_can_product_1000002399_detail_054.png" alt="마일드참치" />
                     <div className="prod-overlay">
-                      부드럽고 고소한 맛의 저유 참치.<br/>
-                      부담 없이 가볍게 즐길 수 있는 일상용 제품입니다.</div>
+                      <strong className='prod-name'>마일드참치</strong>
+                      <p className='prod-desc'>
+                        부드럽고 고소한 맛의 저유 참치.<br/>
+                        부담 없이 가볍게 즐길 수 있는<br/>일상용 제품입니다.
+                      </p>
+                    </div>
                   </div>
                   <div className="prod-img">
                     <img src="./img/products/grid_visual/redtuna_can_product_1000002400_detail_012.png" alt="고추참치" />
                     <div className="prod-overlay">
-                      청양고추의 매콤함이 더해진 참치.<br/>
-                      밥반찬이나 비빔 요리에 매력적인 매운맛을 선사합니다.</div>
+                      <strong className='prod-name'>고추참치</strong>
+                      <p className='prod-desc'> 
+                        청양고추의 매콤함이 더해진 참치.<br/>
+                        밥반찬이나 비빔 요리에<br/>매력적인 매운맛을 선사합니다.
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -290,20 +302,32 @@ function ProductInfo() {
                   <div className="prod-img">
                     <img src="./img/products/grid_visual/basakdolgim_prod_thumb.png" alt="바삭돌김" />
                     <div className="prod-overlay">
-                      청정해역 김을 바삭하게 구워낸 정통 돌김.<br/>
-                      고소한 풍미와 바삭한 식감이 일품입니다.</div>
+                      <strong className='prod-name'>더 고소한 김 바삭 돌김</strong>
+                      <p className='prod-desc'>
+                        청정해역 김을 바삭하게 구워낸 정통 돌김.<br/>
+                        고소한 풍미와 바삭한 식감이 일품입니다.
+                      </p>
+                    </div>
                   </div>
                   <div className="prod-img">
                     <img src="./img/products/grid_visual/chamgirumgim_prod_thumb.png" alt="참기름김" />
                     <div className="prod-overlay">
-                      향긋한 참기름으로 구워낸 풍미 가득한 김.<br/>
-                      밥 반찬은 물론 간식으로도 완벽합니다.</div>
+                      <strong className='prod-name'>더 고소한 김 참기름</strong>
+                      <p className='prod-desc'>
+                        향긋한 참기름으로 구워낸 풍미 가득한 김.<br/>
+                        밥 반찬은 물론 간식으로도 완벽합니다.
+                      </p>
+                    </div>
                   </div>
                   <div className="prod-img">
                     <img src="./img/products/grid_visual/thegosohanparaegim_prod_thumb.png" alt="파래김" style={{ width: "100%" }} />
                     <div className="prod-overlay">
-                      신선한 파래를 더해 감칠맛을 살린 김.<br/>
-                      은은한 향과 부드러운 식감이 조화를 이룹니다.</div>
+                      <strong className='prod-name'>더 고소한 김 파래김</strong>
+                      <p className='prod-desc'>
+                        신선한 파래를 더해 감칠맛을 살린 김.<br/>
+                        은은한 향과 부드러운 식감이 조화를 이룹니다.
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -327,8 +351,11 @@ function ProductInfo() {
                       style={{ width: "90%" }}
                     />
                     <div className="prod-overlay">
-                      게살 본연의 풍미를 살린 프리미엄 맛살.<br/>
-                      샐러드나 초밥 등에도 잘 어울리는 고급형 제품입니다.
+                      <strong className='prod-name'>크라비아</strong>
+                      <p className='prod-desc'>
+                        게살 본연의 풍미를 살린 프리미엄 맛살.<br/>
+                        샐러드나 초밥 등에도 잘 어울리는<br/>고급형 제품입니다.
+                      </p>
                     </div>
                   </div>
                   <div className="prod-img">
@@ -338,8 +365,11 @@ function ProductInfo() {
                       style={{ width: "95%" }}
                     />
                     <div className="prod-overlay">
-                      부드럽고 진한 게살 맛을 느낄 수 있는 풍미 깊은 맛살.<br/>
-                      특별한 식탁을 위한 프리미엄 선택입니다.
+                      <strong className='prod-name'>로얄크랩</strong>
+                      <p className='prod-desc'>
+                        부드럽고 진한 게살 맛을 느낄 수 있는<br/>풍미 깊은 맛살.<br/>
+                        특별한 식탁을 위한 프리미엄 선택입니다.
+                      </p>
                     </div>
                   </div>
                   <div className="prod-img">
@@ -348,8 +378,11 @@ function ProductInfo() {
                       alt="오양맛살"
                     />
                     <div className="prod-overlay">
-                      쫄깃하고 감칠맛이 살아 있는 스테디셀러 맛살.<br/>
-                      언제 어디서나 간편하게 즐길 수 있는 국민 간식입니다.
+                      <strong className='prod-name'>오양맛살</strong>
+                      <p className='prod-desc'>
+                        쫄깃하고 감칠맛이 살아 있는<br/>스테디셀러 맛살.<br/>
+                        언제 어디서나 간편하게 즐길 수 있는<br/>국민 간식입니다.
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -371,8 +404,11 @@ function ProductInfo() {
                       alt="고기손만두"
                     />
                     <div className="prod-overlay">
-                      손으로 빚은 듯한 촉촉한 피와 풍부한 고기 속.<br/>
-                      집밥처럼 따뜻한 정통 손만두의 맛을 담았습니다.
+                      <strong className='prod-name'>고기손만두</strong>
+                      <p className='prod-desc'>
+                        손으로 빚은 듯한 촉촉한 피와 풍부한 고기 속.<br/>
+                        집밥처럼 따뜻한 정통 손만두의 맛을 담았습니다.
+                      </p>
                     </div>
                   </div>
                   <div className="prod-img">
@@ -381,8 +417,11 @@ function ProductInfo() {
                       alt="김치손만두"
                     />
                     <div className="prod-overlay">
-                      아삭한 김치와 고기의 조화가 어우러진 매콤한 손만두.<br/>
-                      깊은 감칠맛으로 입맛을 확 살려줍니다.
+                      <strong className='prod-name'>김치손만두</strong>
+                      <p className='prod-desc'>
+                        아삭한 김치와 고기의 조화가 어우러진<br/>매콤한 손만두.<br/>
+                        깊은 감칠맛으로 입맛을 확 살려줍니다.
+                      </p>
                     </div>
                   </div>
                   <div className="prod-img">
@@ -391,8 +430,11 @@ function ProductInfo() {
                       alt="고기왕교자"
                     />
                     <div className="prod-overlay">
-                      큼직한 크기와 풍성한 육즙이 살아 있는 교자만두.<br/>
-                      바삭하게 구워 먹으면 더욱 맛있습니다.
+                      <strong className='prod-name'>육즙가득 고기 왕교자</strong>
+                      <p className='prod-desc'>
+                        큼직한 크기와 풍성한 육즙이 살아 있는 교자만두.<br/>
+                        바삭하게 구워 먹으면 더욱 맛있습니다.
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -414,8 +456,7 @@ function ProductInfo() {
                     <div className="prod-overlay">
                       <strong className="prod-name">눈다랑어 속살스테이크</strong>
                       <p className="prod-desc">
-                        다랑어 중 눈이 유난히 큰 눈다랑어의 속살은<br />
-                        마블링이 없어 부드러운 식감에 지방질이 적어<br />
+                        눈다랑어의 속살은 마블링이 없어<br />부드러운 식감에 지방질이 적어<br />
                         담백한 풍미가 가득 담겨있는 부위
                       </p>
                     </div>
@@ -425,9 +466,9 @@ function ProductInfo() {
                     <div className="prod-overlay">
                       <strong className="prod-name">눈다랑어 대뱃살</strong>
                       <p className="prod-desc">
-                        다랑어 중 눈이 유난히 큰 눈다랑어의 뱃살은<br />
-                        탐스러운 선홍빛을 띄며 하얀 마블링도 풍부해<br />
-                        쫄깃한 식감과 고소함이 가득한 가성비가 뛰어난 부위
+                        눈다랑어의 뱃살은<br />
+                        탐스러운 선홍빛을 띄며<br />하얀 마블링도 풍부해<br />
+                        쫄깃한 식감과 고소함이 가득한<br />가성비가 뛰어난 부위
                       </p>
                     </div>
                   </div>
